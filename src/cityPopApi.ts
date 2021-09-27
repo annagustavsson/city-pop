7541bdca53e2168ec94386b6a3c8c81455b025ca
annagustavsson <annalovisaellen@gmail.com>
2021-09-27 19:22:03 +0200

    fix: change func and variable names

--- src/cityPopApi.ts
+++ src/cityPopApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-export const getCityPopulation = async (cityName: String) => {
+export const getPopulation = async (location: String) => { // change name to getPopulation
     const userName = process.env.REACT_APP_API_USERNAME
 
     // look up endpoint for https
     try {
-        const res = await axios.get(`http://api.geonames.org/searchJSON?q=${cityName}&maxRows=10&username=${userName}`)
+        const res = await axios.get(`http://api.geonames.org/searchJSON?q=${location}&maxRows=5&username=${userName}`)
         return res.data
     } catch {
         throw new Error("Api call failed") // find appr. error to throw
