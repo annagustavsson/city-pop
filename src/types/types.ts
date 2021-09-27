980f04b7aac2d466b7fb4030d9708917733bd526
annagustavsson <annalovisaellen@gmail.com>
2021-09-27 19:18:27 +0200

    feat: add separate types file

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+export interface GeoInfo {
+    adminCode1: String,
+    lng: number,
+    geonameId: number,
+    toponymName: String,
+    countryId: number,
+    fcl: String,
+    population: number,
+    countryCode: String,
+    name: String,
+    fclName: String[],
+    adminCodes?: Object,
+    countryName: String,
+    fcodeName: String,
+    adminName1: String,
+    lat: number,
+    fcode: String
+}
