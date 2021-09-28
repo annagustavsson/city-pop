05734df23a150ab7426c569699d29affebda3438
annagustavsson <annalovisaellen@gmail.com>
2021-09-28 16:42:24 +0200

    fix: change url to only give cities as result

--- src/cityPopApi.ts
+++ src/cityPopApi.ts
@@ -1,11 +1,14 @@
 import axios from "axios"
 
-export const getPopulation = async (location: String) => { // change name to getPopulation
+export const getPopulation = async (location: String) => {
     const userName = process.env.REACT_APP_API_USERNAME
 
-    // look up endpoint for https
+    // TODO look up endpoint for https
+    // TODO experiment with fuzzy value (0-1)
+
+    // featureCode=PPLA gives cities, featureCode=PPLC gives capital cities
     try {
-        const res = await axios.get(`http://api.geonames.org/searchJSON?q=${location}&maxRows=5&username=${userName}`)
+        const res = await axios.get(`http://api.geonames.org/searchJSON?q=${location}&orderby=population&featureCode=PPLA&featureCode=PPLC&maxRows=10&username=${userName}`)
         return res.data
     } catch {
         throw new Error("Api call failed") // find appr. error to throw
