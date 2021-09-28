be86ceb225c7d89c39301c3ff9b634264cfc7a8f
annagustavsson <annalovisaellen@gmail.com>
2021-09-28 18:29:13 +0200

    fix: add empty last lines + format citycontext

--- src/contexts/CityContext.tsx
+++ src/contexts/CityContext.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {GeoInfo} from "../types/types"
+import { GeoInfo } from "../types/types"
 
 interface Values {
-    cities: Array<GeoInfo>,
-    updateCities: Function
+  cities: Array<GeoInfo>,
+  updateCities: Function
 }
 
 // ! have to check for null on every use
@@ -13,17 +13,17 @@ interface Props {
   children: React.ReactNode
 }
 
-const CityProvider : React.FC<Props> = ({ children }) => {
+const CityProvider: React.FC<Props> = ({ children }) => {
   const [cities, setCities] = React.useState<GeoInfo[]>([]);
 
-  const updateCities = (cityInfo: GeoInfo[] ) => {
-      setCities(cityInfo)
+  const updateCities = (cityInfo: GeoInfo[]) => {
+    setCities(cityInfo)
   }
 
   return (
-      <CityContext.Provider value={{cities: cities, updateCities: updateCities}}>
-          {children}
-      </CityContext.Provider>
+    <CityContext.Provider value={{ cities: cities, updateCities: updateCities }}>
+      {children}
+    </CityContext.Provider>
   )
 
 }
