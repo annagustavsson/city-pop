30f03869825e207dc836fc1f22d901d91c2db83b
annagustavsson <annalovisaellen@gmail.com>
2021-09-28 16:54:45 +0200

    feat: wrap app in context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CityProvider from "./contexts/CityContext"
 import StartPage from "./components/pages/StartPage/StartPage"
 import SearchCountryPage from "./components/pages/SearchCountryPage/SearchCountryPage"
 import SearchCityPage from "./components/pages/SearchCityPage/SearchCityPage"
@@ -11,15 +12,17 @@ import {
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <div>
-          <Route path="/" exact render={() => <StartPage/>}/>
-          <Route path="/search-country" exact render={() => <SearchCountryPage/>}/>
-          <Route path="/search-city" exact render={() => <SearchCityPage/>}/>
-        </div>
-      </Switch>
+    <CityProvider>
+      <Router>
+        <Switch>
+          <div>
+            <Route path="/" exact render={() => <StartPage />} />
+            <Route path="/search-country" exact render={() => <SearchCountryPage />} />
+            <Route path="/search-city" exact render={() => <SearchCityPage />} />
+          </div>
+        </Switch>
       </Router>
+    </CityProvider>
   );
 }
 
