d847fcd1f0736a4def9e15db758192340c1b6d6b
annagustavsson <annalovisaellen@gmail.com>
2021-09-29 10:26:04 +0200

    feat: add route to CountryPage and CityPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import CityProvider from "./contexts/CityContext"
 import StartPage from "./components/pages/StartPage/StartPage"
 import SearchCountryPage from "./components/pages/SearchCountryPage/SearchCountryPage"
 import SearchCityPage from "./components/pages/SearchCityPage/SearchCityPage"
+import CountryPage from "./components/pages/CountryPage/CountryPage"
+import CityPage from "./components/pages/CityPage/CityPage"
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,6 +21,8 @@ function App() {
             <Route path="/" exact render={() => <StartPage />} />
             <Route path="/search-country" exact render={() => <SearchCountryPage />} />
             <Route path="/search-city" exact render={() => <SearchCityPage />} />
+            <Route path="/country" exact render={() => <CountryPage />} />
+            <Route path="/city" exact render={() => <CityPage />} />
           </div>
         </Switch>
       </Router>
