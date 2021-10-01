20d6ad26eb9d7d81e2377144782cd521a5ac020f
annagustavsson <annalovisaellen@gmail.com>
2021-10-01 22:05:41 +0200

    style: add container for buttons

--- src/components/pages/StartPage/StartPage.tsx
+++ src/components/pages/StartPage/StartPage.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import DefaultButton from "../../common/DefaultButton/DefaultButton"
 import styles from "./startPage.module.scss"
 
 interface Props {
-    
+
 }
 
 const StartPage = (props: Props) => {
 
-let history = useHistory()
+    let history = useHistory()
 
-const goToPage = (route: String) => {
-    history.push(`/${route}`)
-}
+    const goToPage = (route: String) => {
+        history.push(`/${route}`)
+    }
 
     return (
         <div className={styles.container}>
-            <DefaultButton handleClick={goToPage} title= "search smh" route={"search-country"}/>
-            <DefaultButton handleClick={goToPage} title= "search smh else" route={"search-city"}/>
+            <div className={styles.buttonContainer}>
+                <DefaultButton handleClick={goToPage} title="search country" route={"search-country"} />
+            </div>
+            <div className={styles.buttonContainer}>
+                <DefaultButton handleClick={goToPage} title="search city" route={"search-city"} />
+            </div>
+
         </div>
     )
 }
