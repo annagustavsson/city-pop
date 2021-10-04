0018b4e7b60d05070de850d11663f1dbc2d5cdb1
annagustavsson <annalovisaellen@gmail.com>
2021-10-04 23:54:33 +0200

    fix: remove comment

--- src/cityPopApi.ts
+++ src/cityPopApi.ts
@@ -7,6 +7,6 @@ export const getPopulation = async (location: String) => {
         const res = await axios.get(`https://secure.geonames.org/searchJSON?q=${location}&orderby=population&featureCode=PPLA&featureCode=PPLC&maxRows=5&username=${userName}`)
         return res.data
     } catch {
-        throw new Error("Api call failed") // find appr. error to throw
+        throw new Error("Api call failed")
     }
 }
