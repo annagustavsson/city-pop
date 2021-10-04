f50f4ca355149cc172a9e4ac25b13b4320ac4f40
annagustavsson <annalovisaellen@gmail.com>
2021-10-04 15:53:59 +0200

    feat: go to startpage on logo click

--- src/components/common/JumboTron/JumboTron.tsx
+++ src/components/common/JumboTron/JumboTron.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 import logo from "../../../resources/images/citypop-logo.png"
 import styles from "./jumboTron.module.scss"
 
 
 const JumboTron = () => {
+
+    let history = useHistory()
+
+    const handleClick = () => {
+        history.push("/")
+    }
+
     return (
-        <div className={styles.flexContainer}>
+        <div onClick={handleClick} className={styles.flexContainer}>
             <img src={logo}></img>
             <span className={styles.heading}>CityPop</span>
         </div>
